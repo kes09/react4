139ac0064f8f3b0563d29cf5030b3b2924f7c574
kes09 <manjgaladzek00@gmail.com>
2023-02-10 08:00:50 +0400

    modified:   src/App.js 	new file:   src/App2.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{Component,useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const generateUsers = ()=>{
+  return [{id:1, name:'john'},
+          {id:2, name:'mark'},
+          {id:3, name:'bob'},
+          {id:4, name:'samanta'},
+          {id:5, name:'eli'},
+          {id:6, name:'sam'},
+          {id:7, name:'nelly'},
+          {id:8, name:'karlos'},
+          {id:9, name:'aurora'},
+          {id:10,name:'dorota'},
+  ];
+};
+
+export default class App extends Component{
+  constructor (props){
+    super(props);
+    this.state = { users:[],showUserlist: false};
+  }
+  handleToggle = () =>{
+    setTimeout(()=>{
+      this.setState({users: generateUsers()});
+    },1000);
+    this.setState(prevState => ({
+      showUserlist: !prevState.showUserlist}));
+  }
+  handleRemoveItem = () => {
+    const users = this.state.users;
+    if (users.length > 0) {
+      const id = Math.floor(Math.random()* users.length);
+      this.setState({ users: users.filter((_, ind) => ind !==id)});
+    }
+  };
+  
+  componentDidUpdate(prevState){
+    if(prevState.users !== this.state.users){
+      document.title = `${this.state.users.length} users left`;
+    }
+  } 
+  render(){
+    console.log("render");
+    return(
+    <div>
+     <button onClick={this.handleToggle}>click</button>
+     <button onClick={this.handleRemoveItem}>Remove</button>
+     {this.state.showUserlist && this.state.users.length >0 ? (<>{this.state.users.map((item)=>{
+     return <h1 key={item.id}>{item.name}</h1>;
+     })}</>
+     ):(<ChildComponent/>)}
     </div>
-  );
+    )
+};
 }
 
-export default App;
+class ChildComponent extends Component{
+  constructor(props){
+    super(props);
+  };
+  componentWillUnmount () {
+    console.log("unmounting");
+  }
+ 
+  render() {
+    return (
+      <h1>Unmount</h1>
+    );
+  }
+}
---
+++ src/App2.js
@@ -0,0 +1,44 @@
+import React,{Component} from 'react';
+
+export default class Array extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+  handleAddItem = () => {
+    const items = this.state.items;
+    const age = Math.floor(Math.random()*(60 - 10 + 1)) + 10;
+    this.setState({ items: [...items, 'User '+ age ] });
+  };
+ 
+  handleRemoveItem = () => {
+    const items = this.state.items;
+    if (items.length > 0) {
+      const id = Math.floor(Math.random()* items.length);
+      this.setState({ items: items.filter((_, ind) => ind !==id)});
+    }
+  };
+  handleUpdateItem=()=>{
+    const age= this.state.age;
+    this.setState({age :Math.floor(Math.random()*(60 - 10 + 1)) + 10})
+  }
+
+  render() {
+    const items = this.state.items;
+    return (
+      <div className="wrapper">
+        <div>
+          List: {items.length} total items.
+        </div>
+        <button onClick={this.handleAddItem}> Add</button>
+        <button onClick={this.handleRemoveItem}>Remove</button>
+        <button onClick={this.handleUpdateItem}>Update</button>
+        <ul>
+          {items.map((item, id) => <li key={id}>{item}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
